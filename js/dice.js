3cfd98ff81d9e6b586209ac1c68eb2785a5e6cf0
Hugo Giraldo <hgxmiami00@gmail.com>
2015-12-09 21:53:42 -0500

    Update 3
    Update 3

---
+++ js/dice.js
@@ -0,0 +1,17 @@
+function dieRoll(){
+    return Math.round(Math.random() * 11 + 1);
+
+}
+
+var bee = document.getElementById("dice-box");
+box.onclick = function(){
+    if (box.src.includes("open") ){
+        box.src = "img/the_die.jpg";
+        document.getElementById("").innerHTML = "";
+    }
+    else {
+        box.src = "img/the_die.jpg";
+        var elem = document.getElementById("");
+        elem.innerHTML = "";
+    }
+};
